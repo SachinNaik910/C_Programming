020fd145d3abd213578fe987a0cb91637300bb43
tata_bobdem <mukul.b@tataelxsi.co.in>
2018-02-26 16:14:24 +0530

    Chapter2

---
+++ CHAPTER2/var_namesize.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+main()
+{
+    char qwertyuiopasdfghjklzxcvbnmmnbvcxz = '5';
+    char qwertyuiopasdfghjklzxcvbnmmnbvczx = '6';
+    int var=1234567890;
+   
+    printf("first var = %d second var = %d\n",qwertyuiopasdfghjklzxcvbnmmnbvcxz,qwertyuiopasdfghjklzxcvbnmmnbvczx);
+    printf("var =%d \n",var);
+    return 0;
+}
