8b8abaf176ee70954cbdd48b64b1017bf83673cf
Nikshith <sachin.naik910@gmail.com>
2018-03-03 19:47:05 +0530

    March 3

---
+++ CHAPTER3/if_else.c
@@ -0,0 +1,15 @@
+/* if -else if without else part*/
+#include <stdio.h>
+
+main()
+{
+	if(0){
+		printf("1\n");
+	}else if(0){
+		printf("2\n");
+	}else if(1){
+		printf("3\n");
+	}
+	
+	return 0;
+}
